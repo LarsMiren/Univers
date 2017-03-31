eb73f8a7288513b5e791dda3030b133829f390ee
LarsMiren <lars.miren@gmail.com>
2017-03-31 05:01:25 +0300

    first6

--- sources/ua/edu/sumdu/j2se/pr5vect/VectorArray.java
+++ sources/ua/edu/sumdu/j2se/pr5vect/VectorArray.java
@@ -38,7 +38,11 @@ public class VectorArray implements ArrayVector {
 
     @Override
     public double[] get() {
-        return arr;
+        double[] a = new double[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            a[i] = arr[i];
+        }
+        return a;
     }
 
     @Override
